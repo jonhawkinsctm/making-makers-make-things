aef6f8f305246385ca4aef6daa38152f7ee3d46d
Robin Scott <robin.scott@comparethemarket.com>
2023-10-30 13:48:56 +0000

    Updated tree files.

---
+++ exercises/k9-data-structures/lists/tree/problem.js
@@ -0,0 +1,55 @@
+class TreeNode {
+    constructor(key, value = key, parent = null) {
+        this.key = key;
+        this.value = value;
+        this.parent = parent;
+        this.children = [];
+    }
+
+    get isLeaf() {
+        return this.children.length === 0;
+    };
+
+    get hasChildren() {
+        return !this.isLeaf;
+    };
+}
+
+export class Tree {
+    constructor(key, value = key) {
+        this.root = new TreeNode(key, value);
+    };
+
+    *preOrderTraversal(node = this.root) {
+        yield node;
+        if (node.children.length) {
+            for (let child of node.children) {
+                yield* this.preOrderTraversal(child);
+            }
+        }
+    };
+
+    *postOrderTraversal(node = this.root) {
+        if (node.children.length) {
+            for (let child of node.children) {
+                yield* this.postOrderTraversal(child);
+            }
+        }
+        yield node;
+    };
+
+    insert(parentNodeKey, key, value = key) {};
+    find(key) {};
+    remove(key) {};
+}
+
+/*
+The Orlov meerkat family tree
+-----------------------------
+Exercise -
+1. complete the insert and find methods to make the first tests go green
+2. When Maiya and her child are disowned after "the mongoose affair", complete the remove
+method and find out who is left in the Orlov meerkat family tree (tests are commented out for this part).
+
+Extra points for an explanation of the use of generator functions
+*/
--- exercises/k9-data-structures/lists/tree/tree.spec.js
+++ exercises/k9-data-structures/lists/tree/tree.spec.js
@@ -1,14 +1,64 @@
-// import { ListTestSuite } from "../shared/test-suite.js";
-import { Tree } from './soultion.js';
+import { Tree } from './problem.js';
 
-describe("tree", () => {
-    test("construction", () => {
-        const tree = new Tree(1, 'AB');
+describe('The Orlov meerkat family tree', () => {
+    let tree;
 
-        tree.insert(1, 11, 'AC');
-        tree.insert(1, 12, 'BC');
-        tree.insert(12, 121, 'BG');
+    beforeAll(() => {
+        tree = new Tree(1, 'Aleksandr');
 
+        tree.insert(1, 11, 'Bogdan');
+        tree.insert(1, 12, 'Maiya');
+        tree.insert(12, 27, 'Oleg');
+        tree.insert(1, 19, 'Vassily');
+    });
+
+    test('tree successfully built', () => {
         expect(tree.root.hasChildren).toBe(true);
     });
+
+    test('Aleksandr is head of the family', () => {
+        expect(tree.root.value).toBe('Aleksandr');
+    });
+
+    test('Maiya has a meerpup', () => {
+        expect(tree.find(12).isLeaf).toBe(false);
+    });
+
+    test('Oleg does not have a meerpup', () => {
+        expect(tree.find(27).isLeaf).toBe(true);
+    });
+
+    test('key 19 is Vassily', () => {
+        expect(tree.find(19).value).toEqual('Vassily');
+    });
+
+    test('All the family using pre-order traversal', () => {
+        expect([...tree.preOrderTraversal()].map(x => x.value)).toEqual(['Aleksandr', 'Bogdan', 'Maiya', 'Oleg', 'Vassily']);
+    });
+
+    test('All the family using post-order traversal', () => {
+        expect([...tree.postOrderTraversal()].map(x => x.value)).toEqual(['Bogdan', 'Oleg', 'Maiya', 'Vassily', 'Aleksandr']);
+    });
+
+    // describe('when Maiya and her child are disowned after "the mongoose affair" ', () => {
+    //     beforeEach(() => {
+    //         tree.remove(12);
+    //     });
+    //
+    //     test('Maiya has been removed from the family tree', () => {
+    //         expect(tree.find(12)).toBe(undefined);
+    //     });
+    //
+    //     test('Oleg has been sent to an orphanage', () => {
+    //         expect(tree.find(27)).toBe(undefined);
+    //     });
+    //
+    //     test('The remaining family using pre-order traversal', () => {
+    //         expect([...tree.preOrderTraversal()].map(x => x.value)).toEqual(['Aleksandr', 'Bogdan', 'Vassily']);
+    //     });
+    //
+    //     test('The remaining family using post-order traversal', () => {
+    //         expect([...tree.postOrderTraversal()].map(x => x.value)).toEqual(['Bogdan', 'Vassily', 'Aleksandr']);
+    //     });
+    // });
 });
